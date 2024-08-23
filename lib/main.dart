c3500547c97c87cb594cdd97f687e4d285cb0782
muhith7 <abdulmuhithnawawi@gmail.com>
2024-08-23 16:01:46 +0700

    feat: Adding an Authentication Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/screens/auth.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Text("Chat"),
+      home: AuthScreen(),
     );
   }
 }
