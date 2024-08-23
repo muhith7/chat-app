dbc301a558b85fb1ad4d99b7cf0cdfdc21e0c85e
muhith7 <abdulmuhithnawawi@gmail.com>
2024-08-23 16:19:39 +0700

    feat: Adding Buttons & Modes to the Authentication

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -8,6 +8,8 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  var _isLogin = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,38 +20,59 @@ class _AuthScreenState extends State<AuthScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                margin: const EdgeInsets.only(
-                  top: 30,
-                  bottom: 20,
-                  left: 20,
-                  right: 20
-                ),
-                width: 200,
-                child: Image.asset('assets/images/chat.png')
-              ),
+                  margin: const EdgeInsets.only(
+                      top: 30, bottom: 20, left: 20, right: 20),
+                  width: 200,
+                  child: Image.asset('assets/images/chat.png')),
               Card(
                 margin: const EdgeInsets.all(20),
                 child: SingleChildScrollView(
-                  child: Padding(padding: const EdgeInsets.all(16),
-                  child: Form(child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: "Email Address",
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Form(
+                        child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: "Email Address",
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          autocorrect: false,
+                          textCapitalization: TextCapitalization.none,
                         ),
-                        keyboardType: TextInputType.emailAddress,
-                        autocorrect: false,
-                        textCapitalization: TextCapitalization.none,
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: "Password",
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: "Password",
+                          ),
+                          obscureText: true,
                         ),
-                        obscureText: true,
-                      )
-                    ],
-                  )),),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .primaryContainer),
+                            child: Text(_isLogin ? "Login" : "Sign-Up")),
+                        TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            child: Text(_isLogin
+                                ? 'Create an Account'
+                                : 'I Already have an account'))
+                      ],
+                    )),
+                  ),
                 ),
               )
             ],
