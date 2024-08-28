f107563347872b9cd1839ea5129cae9c5a02276d
muhith7 <abdulmuhithnawawi@gmail.com>
2024-08-28 21:02:41 +0700

    feat: Sending Data to Firestore

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:chat_app/widgets/user_image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -52,7 +53,15 @@ class _AuthScreenState extends State<AuthScreen> {
 
         await storageRef.putFile(selectedImage!);
         final imageUrl = await storageRef.getDownloadURL();
-        print(imageUrl);
+
+        FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCridentials.user!.uid)
+            .set({
+          'username': 'to be done ..',
+          'email': _enteredEmail,
+          'image_url': imageUrl,
+        });
       }
     } on FirebaseAuthException catch (error) {
       if (error.code == 'email-already-in-use') {
